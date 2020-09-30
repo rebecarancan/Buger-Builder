cbb90de8819e693b60536e392582a693749c31f2
Rebeca Rancan <rebecarancanrr@gmail.com>
2020-09-30 17:18:55 -0300

    Sending a POST Request

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseUrl: 'https://react-burger-builder-20792.firebaseio.com/'
+  baseURL: 'https://react-burger-builder-20792.firebaseio.com'
 });
 
 export default instance;
