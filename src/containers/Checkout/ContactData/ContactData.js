a1aabc35eab5ae05f652326d26f2bf46391c0199
Rebeca Rancan <rebecarancanrr@gmail.com>
2021-01-29 09:05:02 -0300

    Working on Order Actions

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -92,7 +92,6 @@ class ContactData extends Component {
         valid: true
       }
     },
-    loading: false,
     isValid: false
   }
 
@@ -173,7 +172,7 @@ class ContactData extends Component {
         <Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button>
       </form>
     );
-    if (this.state.loading) {
+    if (this.props.loading) {
       form = <Spinner />;
     }
     return (
@@ -188,12 +187,15 @@ class ContactData extends Component {
 const mapStateToProps = state => {
   return {
     ings: state.ingredients,
-    price: state.totalPrice
+    price: state.totalPrice,
+    loading: state.loading
   }
 };
 
 const mapDispatchToProps = dispatch => {
-  onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart())
+  return {
+    onOrderBurger: (orderData) => dispatch(actions.purchaseBurger())
+  };
 };
 
-export default connect(mapStateToProps)(withErrorHandler(ContactData, axios));
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios));
