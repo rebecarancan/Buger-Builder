9d089103b14c39c61aa647f548a3f4ef57a7fbfa
Rebeca Rancan <rebecarancanrr@gmail.com>
2021-02-04 09:02:31 -0300

    improve reducers

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -12,10 +12,10 @@ const INGREDIENT_PRICES = {
   cheese: 0.4,
   meat: 1.3,
   bacon: 0.7
-}
+};
 
-const addIgredient = (state, action) => {
-  const updatedIngredient = { [action.ingredientName]: state.ingredients[action.ingredientName] + 1 };
+const addIngredient = (state, action) => {
+  const updatedIngredient = { [action.ingredientName]: state.ingredients[action.ingredientName] + 1 }
   const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
   const updatedState = {
     ingredients: updatedIngredients,
@@ -24,39 +24,39 @@ const addIgredient = (state, action) => {
   return updateObject(state, updatedState);
 };
 
-const removeIgredient = (state, action) => {
+const removeIngredient = (state, action) => {
   const updatedIng = { [action.ingredientName]: state.ingredients[action.ingredientName] - 1 };
   const updatedIngs = updateObject(state.ingredients, updatedIng);
   const updatedSt = {
     ingredients: updatedIngs,
-    totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+    totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
   }
   return updateObject(state, updatedSt);
 };
 
-const setIgredients = (state, action) => {
+const setIngredients = (state, action) => {
   return updateObject(state, {
     ingredients: {
       salad: action.ingredients.salad,
       bacon: action.ingredients.bacon,
       cheese: action.ingredients.cheese,
-      meat: action.ingredients.meat,
+      meat: action.ingredients.meat
     },
     totalPrice: 4,
     error: false
   });
 };
 
-const fetchIgredientsFailed = (state, action) => {
+const fetchIngredientsFailed = (state, action) => {
   return updateObject(state, { error: true });
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.ADD_INGREDIENT: return addIgredient(state, action);
-    case actionTypes.REMOVE_INGREDIENT: return removeIgredient(state, action);
-    case actionTypes.SET_INGREDIENTS: return setIgredients(state, action);
-    case actionTypes.FETCH_INGREDIENTS_FAILED: return fetchIgredientsFailed(state, action);
+    case actionTypes.ADD_INGREDIENT: return addIngredient(state, action);
+    case actionTypes.REMOVE_INGREDIENT: return removeIngredient(state, action);
+    case actionTypes.SET_INGREDIENTS: return setIngredients(state, action);
+    case actionTypes.FETCH_INGREDIENTS_FAILED: return fetchIngredientsFailed(state, action);
     default: return state;
   }
 };
--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -12,7 +12,7 @@ const purchaseInit = (state, action) => {
 };
 
 const purchaseBurgerStart = (state, action) => {
-  return updateObject(state, { loading: true });
+  return updateObject(state, { loading: false });
 };
 
 const purchaseBurgerSuccess = (state, action) => {
@@ -48,7 +48,7 @@ const reducer = (state = initialState, action) => {
     case actionTypes.PURCHASE_INIT: return purchaseInit(state, action);
     case actionTypes.PURCHASE_BURGER_START: return purchaseBurgerStart(state, action);
     case actionTypes.PURCHASE_BURGER_SUCCESS: return purchaseBurgerSuccess(state, action);
-    case  actionTypes.PURCHASE_BURGER_FAIL: return purchaseBurgerFail(state, action);
+    case actionTypes.PURCHASE_BURGER_FAIL: return purchaseBurgerFail(state, action);
     case actionTypes.FETCH_ORDERS_START: return fetchOrdersStart(state, action);
     case actionTypes.FETCH_ORDERS_SUCCESS: return fetchOrdersSuccess(state, action);
     case actionTypes.FETCH_ORDERS_FAIL: return fetchOrdersFail(state, action);
